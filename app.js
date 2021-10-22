d8f0a4f3823605c59804cd7b0d28e6435673fc57
Haoran-Cui <67232288+Haoran-Cui@users.noreply.github.com>
2021-10-22 16:32:46 -0400

    frontend: port 80 -> 8080

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 80
+const port = 8080
 const path = require('path');
 app.use(express.static('public'))
 var cors = require('cors')
