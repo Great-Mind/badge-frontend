e211c079f6a16f24fb72e7374b584f69fca4f635
Haoran-Cui <67232288+Haoran-Cui@users.noreply.github.com>
2021-11-14 15:46:26 -0500

    frontend: localhost -> 128.253.128.12

--- app.js
+++ app.js
@@ -72,7 +72,7 @@ app.post("/login", function (req, res, next) {
   var email = req.body.email;
   var passwd = req.body.passwd;
   // $.ajax({
-  //     url:`http://localhost:8080/login?email=${email}&passwd=${passwd}`,
+  //     url:`http://128.253.128.12:8080/login?email=${email}&passwd=${passwd}`,
   //     type: 'POST',
   //     success: function(result) {
   //         if (result) {
@@ -87,10 +87,9 @@ app.post("/login", function (req, res, next) {
   //             res.json({res: 1, msg: 'Password is not correct'});
   //         }
   //     }
-
   // });
 
-  var url = `http://localhost:8080/user/login?email=${email}&passwd=${passwd}`;
+  var url = `http://128.253.128.12:8080/user/login?email=${email}&passwd=${passwd}`;
   request.post(url, function (error, response, body) {
     if (!error && response.statusCode == 200) {
       console.log(body);
@@ -113,10 +112,6 @@ app.post("/login", function (req, res, next) {
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "./public", "/login.html"));
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
 });
 
 app.get("/home", function (req, res) {
@@ -149,4 +144,8 @@ app.get("/relationship", function (req, res) {
 
 app.get("/group", function (req, res) {
   res.sendFile(path.join(__dirname, "./public", "/group.html"));
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://128.253.128.12:${port}`);
 });
